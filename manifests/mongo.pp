bc04b9619aa3323a4bd405dc1fa580076a4fb0fa
Marco Leong <marcogood411@gmail.com>
2013-02-07 17:56:03 +0800

    fix provision files

--- manifests/mongo.pp
+++ manifests/mongo.pp
@@ -2,32 +2,25 @@ class mongo {
 
     package { "mongodb-10gen":
         ensure => present,
-        require => Exec["apt-key mongokey"]
+        require => [Apt::Key["mongo"], Apt::Source["10gen"]]
     }
 
-    # sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-    exec { 'apt-key mongokey':
-        command => '/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10',
-        before => File["/etc/apt/sources.list.d/10gen.list"]
+    apt::key { "mongo":
+        key => "7F0CEB10",
+        key_server => "keyserver.ubuntu.com"
     }
 
-    exec { 'apt-get update':
-        command => '/usr/bin/apt-get update',
-        before => Package["mongodb-10gen"],
-        require => File["/etc/apt/sources.list.d/10gen.list"]
+    apt::source { "10gen" :
+        location => "http://downloads-distro.mongodb.org/repo/ubuntu-upstart",
+        release => "dist",
+        repos => "10gen",
+        include_src => false,
+        require => Apt::Key["mongo"]
     }
 
     service { "mongodb":
         ensure => running,
         require => Package["mongodb-10gen"]
-    }
-
-    file { '/etc/apt/sources.list.d/10gen.list':
-        owner  => root,
-        group  => root,
-        ensure => file,
-        mode   => 644,
-        source => '/vagrant/files/apt/10gen.list'
     }
 }
     
